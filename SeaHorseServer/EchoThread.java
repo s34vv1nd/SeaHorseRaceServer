dd04a161ac9baf5355020a49677894939f9a7d34
Hai Phong <aquariuslee99@gmail.com>
2021-04-05 17:23:14 +0700

    Add Echo Server

--- SeaHorseServer/EchoThread.java
+++ SeaHorseServer/EchoThread.java
@@ -3,41 +3,40 @@ package SeaHorseServer;
 import java.io.*;
 import java.net.*;
 
-
 public class EchoThread extends Thread {
   protected Socket socket;
 
   public EchoThread(Socket clientSocket) {
-      this.socket = clientSocket;
+    this.socket = clientSocket;
   }
 
   public void run() {
-      InputStream inp = null;
-      BufferedReader brinp = null;
-      DataOutputStream out = null;
+    InputStream inp = null;
+    BufferedReader brinp = null;
+    DataOutputStream out = null;
+    try {
+      inp = socket.getInputStream();
+      brinp = new BufferedReader(new InputStreamReader(inp));
+      out = new DataOutputStream(socket.getOutputStream());
+    } catch (IOException e) {
+      return;
+    }
+    String line;
+    while (true) {
       try {
-          inp = socket.getInputStream();
-          brinp = new BufferedReader(new InputStreamReader(inp));
-          out = new DataOutputStream(socket.getOutputStream());
-      } catch (IOException e) {
+        line = brinp.readLine();
+        System.out.println(line);
+        if ((line == null) || line.equalsIgnoreCase("QUIT")) {
+          socket.close();
           return;
+        } else {
+          out.writeBytes(line + "\r");
+          out.flush();
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+        return;
       }
-      String line;
-      while (true) {
-          try {
-              line = brinp.readLine();
-              System.out.println(line);
-              if ((line == null) || line.equalsIgnoreCase("QUIT")) {
-                  socket.close();
-                  return;
-              } else {
-                  out.writeBytes(line + "\r");
-                  out.flush();
-              }
-          } catch (IOException e) {
-              e.printStackTrace();
-              return;
-          }
-      }
+    }
   }
 }
--- SeaHorseServer/ThreadedEchoServer.java
+++ SeaHorseServer/ThreadedEchoServer.java
@@ -3,29 +3,28 @@ package SeaHorseServer;
 import java.io.*;
 import java.net.*;
 
-
 public class ThreadedEchoServer {
 
-  static final int PORT = 5000;
+    static final int PORT = 5000;
 
-  public static void main(String args[]) {
-      ServerSocket serverSocket = null;
-      Socket socket = null;
+    public static void main(String args[]) {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
 
-      try {
-          serverSocket = new ServerSocket(PORT);
-          System.out.println("Server is running on port " + PORT);
-      } catch (IOException e) {
-          e.printStackTrace();
-      }
-      while (true) {
-          try {
-              socket = serverSocket.accept();
-          } catch (IOException e) {
-              System.out.println("I/O error: " + e);
-          }
-          // new thread for a client
-          new EchoThread(socket).start();
-      }
-  }
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Server is running on port " + PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("I/O error: " + e);
+            }
+            // new thread for a client
+            new EchoThread(socket).start();
+        }
+    }
 }
